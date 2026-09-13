dfaa501478d44455e7cafd812a79935a86041281
div5268singh <divyanshu.developerbrothers@gmail.com>
2026-09-13 16:27:49 +0530

    Create Consutment Changes

--- server/SantaRoad.Api/Program.cs
+++ server/SantaRoad.Api/Program.cs
@@ -8,7 +8,15 @@ builder.Services
     .AddApiDocumentation(builder.Configuration)
     .AddPersistence(builder.Configuration)
     .AddJwtAuthentication(builder.Configuration)
-    .AddCorsPolicy(builder.Configuration, builder.Environment);
+    .AddCorsPolicy(builder.Configuration, builder.Environment)
+    .AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+        policy.WithOrigins(allowedOrigins)
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials()); // only if you send cookies; omit if using bearer tokens only
+});
 
 var app = builder.Build();
 
@@ -16,5 +24,8 @@ await app.InitializeDatabaseAsync();
 
 app.UseApiPipeline();
 app.MapApiEndpoints();
+app.UseCors("AllowFrontend");
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.Run();
